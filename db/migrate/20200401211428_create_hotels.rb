65216179202ea58db53eff4172a9d67867b5a328
andrewsilva9 <andrewsilva9@gmail.com>
2020-04-02 19:57:47 -0600

    created migrations

--- db/migrate/20200401211428_create_hotels.rb
+++ db/migrate/20200401211428_create_hotels.rb
@@ -1,6 +1,11 @@
 class CreateHotels < ActiveRecord::Migration[6.0]
   def change
     create_table :hotels do |t|
+      t.string :name
+      t.integer :price_level
+      t.text :amenities
+      t.belongs_to :location, foreign_key: true
+      t.belongs_to :user, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20200401211437_create_reviews.rb
+++ db/migrate/20200401211437_create_reviews.rb
@@ -1,6 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
+      t.date :date_visited
+      t.text :content
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :hotel, foreign_key: true
 
       t.timestamps
     end
