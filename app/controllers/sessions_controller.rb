d7fabfa8d4cd4b606119c3b3f25389d656a461a5
andrewsilva9 <andrewsilva9@gmail.com>
2020-04-07 13:00:57 -0600

    added login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,16 @@ class SessionsController < ApplicationController
 
     def welcome
     end
+
+    def new
+    end
+
+    def create
+      if params[:username].nil? || params[:username].empty?
+       redirect_to :login
+      else
+       session[:username] = params[:username]
+       redirect_to :show
+      end
+    end
 end
