8f9d042a69789fc7f585a9d7ff7ac50f7bada4bd
andrewsilva9 <andrewsilva9@gmail.com>
2020-04-08 14:52:30 -0600

    still working through authenticating with Google

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,16 +7,11 @@ class SessionsController < ApplicationController
     end
 
     def google_create
-        @user = User.find_or_create_by(uid: auth['uid']) do |u|
-            u.name = auth['info']['name']
-            u.username = auth['info']['email']
-            u.password = 'password'
-            u.password_confirmation = 'password'
-          end
-          @user.save
-          session[:user_id] = @user.id
+        @user = User.from_omniauth(request.env['omniauth.auth'])
+        @user.save
+        session[:user_id] = @user.id
       
-          redirect_to user_path(@user)
+        redirect_to user_path(@user)
     end
 
     def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
     validates :username, uniqueness: true, presence: true
     has_many :reviews
     has_many :hotels, through: :reviews
+
+    def self.from_omniauth(auth_hash)
+        user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
+        user.username = auth_hash['info']['name']
+        user.location = auth_hash['info']['location']
+        user.image_url = auth_hash['info']['image']
+        user.url = auth_hash['info']['urls']['Google']
+        user.save!
+        user
+      end
+
+    
 end
