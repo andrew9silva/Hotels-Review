5f64cab91d9fa03dbcf9acdf130027bd9ac27ce2
andrewsilva9 <andrewsilva9@gmail.com>
2020-04-15 16:31:43 -0600

    added review controllers and views, not able to submit review yet

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,29 @@
 class ReviewsController < ApplicationController
+
+    def index
+        @reviews = Review.all
+    end
+
+    def new
+        @review = Review.new
+    end
+
+    def create
+        @review = Review.new(review_params)
+        if @review.save!
+           redirect_to review_path(@review)
+        else
+           redirect_to '/reviews/new'
+        end  
+    end
+
+    def show
+        @review = Review.find_by_id(params[:id])
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:date_visited, :content)
+    end
 end
